3c0b5cd4fd6c1ce3b8aed7ad23435a479b8bb574
Pranshu Patel <pranshu05patel@gmail.com>
2024-12-05 23:08:30 +0530

    feat: update UserData component to adjust image size for better display on user profile

--- src/components/(user)/UserData.jsx
+++ src/components/(user)/UserData.jsx
@@ -5,7 +5,7 @@ export default function UserData({ session, user, userId }) {
     console.log(user)
     return (
         <div className="flex flex-col items-center h-full w-full p-4 bg-[#121212] rounded-lg">
-            <img src={user.image === 'unknown' ? 'https://github.com/user-attachments/assets/bf57cb96-b259-4290-b35b-0ede9d618802' : user.image} alt={user.name} className="w-32 h-32 lg:w-1/2 lg:h-auto rounded-full mb-4" />
+            <img src={user.image === 'unknown' ? 'https://github.com/user-attachments/assets/bf57cb96-b259-4290-b35b-0ede9d618802' : user.image} alt={user.name} className="w-32 h-32 lg:w-48 lg:h-48 rounded-full mb-4" />
             <h1 className="text-2xl lg:text-4xl font-bold mb-4">{user.name}</h1>
             {session?.user?.id === userId && (<button onClick={() => signOut()} className="px-4 py-2 bg-[#121212] border-[2px] border-[#333] rounded-md">Sign Out</button>)}
         </div>
