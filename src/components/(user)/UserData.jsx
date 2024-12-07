f4421a586e3ac4b9f4a0885c3e2a6ad6dc1fd043
Pranshu Patel <pranshu05patel@gmail.com>
2024-12-07 12:55:59 +0530

    feat: display user points in UserData component

--- src/components/(user)/UserData.jsx
+++ src/components/(user)/UserData.jsx
@@ -6,6 +6,7 @@ export default function UserData({ session, user, userId }) {
         <div className="flex flex-col items-center w-full p-3 bg-[#121212] rounded-lg">
             <img src={user.image === 'unknown' ? 'https://github.com/user-attachments/assets/bf57cb96-b259-4290-b35b-0ede9d618802' : user.image} alt={user.name} className="w-32 h-32 lg:w-52 lg:h-52 rounded-full mb-4 object-cover" />
             <h1 className="text-2xl lg:text-3xl font-bold mb-4">{user.name}</h1>
+            <p className="text-sm lg:text-base text-[#888] mb-4">{user.points} TuneStats Points</p>
             <div className="w-full flex justify-center gap-2 text-sm">
                 <a href={`https://open.spotify.com/user/${userId}`} target="_blank" rel="noreferrer" className="px-4 py-2 bg-[#1DB954] rounded-md">Spotify</a>
                 {session?.user?.id === userId && (<button onClick={() => signOut()} className="px-4 py-2 bg-[#121212] border-[2px] border-[#333] rounded-md">Sign Out</button>)}
