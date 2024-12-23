8529ae36492f109a99a944995c5c2d72dd7fe9ae
Pranshu Patel <pranshu05patel@gmail.com>
2024-12-23 18:21:36 +0530

    feat: implement API endpoint to fetch track details from Spotify and create TrackPage component

---
+++ src/pages/track/[trackId]/index.jsx
@@ -0,0 +1,7 @@
+export default function TrackPage({ track }) {
+    return (
+        <div className="text-center min-h-screen flex flex-col items-center justify-center">
+           <h1 className="text-4xl font-bold">Coming Soon!</h1>
+        </div>
+    );
+}
